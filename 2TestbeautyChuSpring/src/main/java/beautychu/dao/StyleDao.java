6ad2fadd099528e6fbe3951b83fe4a505ec47ec2
k <inblue17@gmail.com>
2015-01-15 10:22:11 +0900

    디비에서 불러오기 성공!

--- 2TestbeautyChuSpring/src/main/java/beautychu/dao/StyleDao.java
+++ 2TestbeautyChuSpring/src/main/java/beautychu/dao/StyleDao.java
@@ -3,9 +3,14 @@ package beautychu.dao;
 import java.util.List;
 import java.util.Map;
 
+import beautychu.domain.Member;
+import beautychu.domain.Style;
+
 public interface StyleDao {
 	
 	  List<?> getList();
-	
+	  List<?> getGridList();
+	  /*Style existUser(Map<String,String> params);*/
+	  void insertStyle(Style style);
 
 	}
--- 2TestbeautyChuSpring/src/main/java/beautychu/service/StyleService.java
+++ 2TestbeautyChuSpring/src/main/java/beautychu/service/StyleService.java
@@ -1,11 +1,13 @@
 package beautychu.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import beautychu.dao.StyleDao;
+import beautychu.domain.Member;
 import beautychu.domain.Style;
 
 @Service
@@ -13,8 +15,24 @@ public class StyleService {
   @Autowired StyleDao styleDao;
   
   public List<?> getList() {
-    return styleDao.getList(); 
-  }
+	    return styleDao.getList(); 
+	  }
+  
+  public List<?> getGridList() {
+	    return styleDao.getGridList(); 
+	  }
+  
+  
+/*	public Style validate(String email, String password) {
+		HashMap<String, String> params = new HashMap<>();
+		params.put("email", email);
+		params.put("password", password);
+		return styleDao.existUser(params);
+	}*/
+
+	public void insertStyle(Style style) {
+		styleDao.insertStyle(style);
+	}
 }
 
 
